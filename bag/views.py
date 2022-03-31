5a4d2b53dbeae0491136575bc468053dd6f05c7e
dkelly255 <dkelly25@hotmail.co.uk>
2022-03-31 13:18:04 +0000

    Added bag context and delivery logic

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def view_bag(request):
-    """A view that ernders the bag contents screen"""
+    """A view that renders the bag contents screen"""
     
     return render(request, 'bag/bag.html')
